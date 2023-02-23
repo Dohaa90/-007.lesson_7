dcff8449d1b3c4613330b81c2be47758c1ef901d
Mark Moskalevskij <dohaa@ukr.net>
2023-02-23 02:57:46 +0200

    add homework

---
+++ l7.js
@@ -0,0 +1,7 @@
+let firstNumber = prompt('Введите число 1');
+let secondNumber = prompt('Введите число 2');
+let thirthNumber = prompt('Введите число 3');
+
+let mean = (Number(firstNumber) + Number(secondNumber) + Number(thirthNumber)) / 3;
+
+alert(mean)
